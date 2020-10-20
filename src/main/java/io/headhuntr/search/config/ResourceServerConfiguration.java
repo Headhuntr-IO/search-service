abbb0273b94d8e756450b7e6711b6a61bbbfb004
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-20 17:54:13 +0800

    add health check endpoint for the helm test (post install) as well as to help k8 decide whether to spawn healthier instances

--- src/main/java/io/headhuntr/search/config/ResourceServerConfiguration.java
+++ src/main/java/io/headhuntr/search/config/ResourceServerConfiguration.java
@@ -2,8 +2,18 @@ package io.headhuntr.search.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerConfiguration {
+public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .antMatchers("/actuator/health").permitAll()
+                        .anyRequest().authenticated());
+    }
 }
