83d623ce4ee07a9ba24138952f402329b3095282
LM Bibera <lyndon.bibera@headhuntr.io>
2020-11-05 16:02:49 +0800

    add open-api autogenerated docs

--- src/main/java/io/headhuntr/search/config/ResourceServerConfiguration.java
+++ src/main/java/io/headhuntr/search/config/ResourceServerConfiguration.java
@@ -14,7 +14,7 @@ public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> authorizeRequests
-                        .antMatchers("/actuator/health").permitAll()
+                        .antMatchers("/actuator/health", "/swagger-ui/**", "/v3/api-docs**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
