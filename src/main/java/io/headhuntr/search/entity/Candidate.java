c0a53f066c31c8422c37f4bafc3bf79d0e9a6a40
LM Bibera <lyndon.bibera@headhuntr.io>
2020-11-06 13:03:17 +0800

    more documentation

--- src/main/java/io/headhuntr/search/entity/Candidate.java
+++ src/main/java/io/headhuntr/search/entity/Candidate.java
@@ -1,22 +1,56 @@
 package io.headhuntr.search.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.util.List;
 
+@Schema(
+        name = "Candidate",
+        description = "Cool people that we need to hire"
+)
 @Data
 @Document(indexName = "hh2_candidates")
 public class Candidate {
 
     @Id
+    @Schema(
+            description = "The unique identifier of a candidate",
+            example = "100000",
+            required = true
+    )
     String id;
 
+    @Schema(
+            description = "Given name of the candidate",
+            example = "John",
+            required = true
+    )
     String firstName;
+
+    @Schema(
+            description = "Family name of the candidate",
+            example = "Doe",
+            required = true
+    )
     String lastName;
+
+    @Schema(
+            description = "The complete name of the candidate",
+            example = "John Doe",
+            required = true
+    )
     String fullName;
+
+    @Schema(
+            description = "The total work experience expressed in months to simplify things",
+            example = "18",
+            required = true
+    )
     Integer monthsExperience;
+
     List<ContactInformation> contactDetails;
 
     List<Job> jobHistory;
@@ -26,6 +60,11 @@ public class Candidate {
     @Data
     public static class ContactInformation {
 
+        @Schema(
+                description = "The order of the contact information, lower sequence should be shown first",
+                example = "1",
+                required = true
+        )
         Integer sequence;
 
         /**
--- src/main/java/io/headhuntr/search/repository/CandidateRepository.java
+++ src/main/java/io/headhuntr/search/repository/CandidateRepository.java
@@ -2,9 +2,15 @@ package io.headhuntr.search.repository;
 
 import com.amazonaws.xray.spring.aop.XRayEnabled;
 import io.headhuntr.search.entity.Candidate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.repository.query.Param;
 
 @Tag(
         name = "Candidate Resource",
@@ -13,4 +19,17 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 @SecurityRequirement(name = "api")
 @XRayEnabled
 public interface CandidateRepository extends ElasticsearchRepository<Candidate, String> {
+
+    @Operation(
+            operationId = "Candidate.findAllByFirstNameOrLastName",
+            description = "Search for candidates that partially match with the first or last name",
+            tags = { "candidate" }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found results"
+            )
+    })
+    Page<Candidate> findAllByFirstNameOrLastName(@Param("name") String name, Pageable page);
 }
